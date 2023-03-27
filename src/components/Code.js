91cbcda671983a6c31e995e355c1a8e6bfd3ca5b
Aayush <just.another.3d.object@gmail.com>
2023-03-27 23:52:28 +0530

    Changed javascript link for output code

--- src/components/Code.js
+++ src/components/Code.js
@@ -216,7 +216,7 @@ const Code = ({
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
-    <script src="https://startbootstrap.github.io/startbootstrap-resume/js/scripts.js"></script>
+    <script src="https://github.com/JustA3DObject/profia-portfolio-generator/blob/main/src/components/Javascript.js"></script>
   </body>
 </html>
 `}
